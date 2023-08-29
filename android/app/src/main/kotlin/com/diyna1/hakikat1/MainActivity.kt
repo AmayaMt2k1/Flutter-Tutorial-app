5abe39b7b508f1c61e4bc204d7e3e4b1bbc8111f
Amanda <daniasafiyah68@gmail.com>
2023-08-29 09:24:30 +0800

    description

---
+++ android/app/src/main/kotlin/com/diyna1/hakikat1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.diyna1.hakikat1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/description_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:hakikat1/class/item_class.dart';
+import 'package:hakikat1/core/constants.dart';
+
+class DescriptionPage extends StatefulWidget {
+  const DescriptionPage({
+    super.key,
+    required this.box,
+  });
+
+  final ItemClass box;
+
+  @override
+  State<DescriptionPage> createState() => _DescriptionPageState();
+}
+
+class _DescriptionPageState extends State<DescriptionPage> {
+  double FontSizeCustom = 25;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.box.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Snackbar'),
+                  behavior: SnackBarBehavior.floating,
+                ),
+              );
+            },
+            icon: const Icon(Icons.info),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(kDouble10),
+          child: Column(
+            children: [
+              Image.asset(widget.box.imagePath),
+              Wrap(
+                spacing: kDouble10,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        FontSizeCustom = 25;
+                      });
+                    },
+                    child: const Text('Small Title'),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      setState(() {
+                        FontSizeCustom = 35;
+                      });
+                    },
+                    child: const Text('Medium Title'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        FontSizeCustom = 50;
+                      });
+                    },
+                    child: const Text('Large Title'),
+                  ),
+                  FilledButton(
+                    onPressed: () {
+                      setState(() {
+                        FontSizeCustom = 200;
+                      });
+                    },
+                    child: const Text('Huge Title'),
+                  ),
+                ],
+              ),
+              FittedBox(
+                child: Text(
+                  widget.box.title,
+                  style: TextStyle(
+                    fontSize: FontSizeCustom,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Text(
+                rocketDescription,
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(
+                height: kDouble10,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
